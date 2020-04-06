d5a401b188d0f0c05268059cf0d5825074d2db5c
hemilioaraujo <hemilioaraujo@gmail.com>
2020-04-06 16:24:27 -0300

    transformado acesso a banco de dados em classe

--- arquivos_para_teste/bd.py
+++ arquivos_para_teste/bd.py
@@ -1,84 +1,75 @@
 import mysql.connector
 from mysql.connector import errorcode
 
+class ConectorBD:
+    def __init__(self):
+        self._conexao = None
+        self._cursor = None
 
-def pesquisa_ambiental_linha2_diario(data):
-    try:
-        db_connection = mysql.connector.connect(host='10.10.10.200', user='root', password='sa', database='nideal')
-        print("Database connection made!")
-        cursor = db_connection.cursor()
-        cursor.callproc("PesquisaAmbientalLinha2Diario", [f'{data}',])
 
-        for result in cursor.stored_results():
+    def conectar(self):
+        try:
+            conn = mysql.connector.connect(host='10.10.10.200', user='root', password='sa',
+                                                    database='nideal')
+            self._conexao = conn
+            self._cursor = conn.cursor()
+            print('Conectado!')
+        except mysql.connector.Error as error:
+            if error.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database doesn't exist")
+            elif error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("User name or password is wrong")
+            else:
+                print(error)
+
+
+    def desconectar(self):
+        self._cursor.close()
+        self._conexao.close()
+        print('Desconectado!')
+
+
+    def pesquisa_ambiental_linha2_diario(self, data):
+        self.conectar()
+        self._cursor.callproc("PesquisaAmbientalLinha2Diario", [f'{data}', ])
+
+        for result in self._cursor.stored_results():
             # print(result.fetchall())
             for i in result:
                 print(i)
-        print("ok")
 
-    except mysql.connector.Error as error:
-        if error.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database doesn't exist")
-        elif error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("User name or password is wrong")
-        else:
-            print(error)
-    else:
-        db_connection.close()
+        self.desconectar()
 
 
-def pesquisa_ambiental_linha2_lme():
-    try:
-        db_connection = mysql.connector.connect(host='10.10.10.200', user='root', password='sa',
-                                                database='nideal')
-        print("Database connection made!")
-        cursor = db_connection.cursor()
-        cursor.callproc("PesquisaAmbientalLinha2_LME", [])
+    def pesquisa_ambiental_linha2_lme(self):
+        self.conectar()
+        self._cursor.callproc("PesquisaAmbientalLinha2_LME", [])
 
-        for result in cursor.stored_results():
+        for result in self._cursor.stored_results():
             # print(result.fetchall())
             for i in result:
                 print(i)
-        print("ok")
 
-    except mysql.connector.Error as error:
-        if error.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database doesn't exist")
-        elif error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("User name or password is wrong")
-        else:
-            print(error)
-    else:
-        db_connection.close()
+        self.desconectar()
 
+    def pesquisa_ambiental_linha2_diario_total(self, data):
+        self.conectar()
+        self._cursor.callproc("PesquisaAmbientalLinha2Diario_Total", [f'{data}', ])
 
-def pesquisa_ambiental_linha2_diario_total(data):
-    try:
-        db_connection = mysql.connector.connect(host='10.10.10.200', user='root', password='sa',
-                                                database='nideal')
-        print("Database connection made!")
-        cursor = db_connection.cursor()
-        cursor.callproc("PesquisaAmbientalLinha2Diario_Total", [f'{data}',])
-
-        for result in cursor.stored_results():
+        for result in self._cursor.stored_results():
             # print(result.fetchall())
             for i in result:
                 print(i)
-        print("ok")
 
-    except mysql.connector.Error as error:
-        if error.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database doesn't exist")
-        elif error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("User name or password is wrong")
-        else:
-            print(error)
-    else:
-        db_connection.close()
+        self.desconectar()
 
 
 if __name__ == "__main__":
-    # pesquisa_ambiental_linha2_diario('2020-04-01')
-    # pesquisa_ambiental_linha2_lme()
-    pesquisa_ambiental_linha2_diario_total('2020-04-01')
-
-
+    con = ConectorBD()
+    # con.conectar()
+    # print('conectou')
+    # con.desconectar()
+    # print('desconectou')
+    # con.pesquisa_ambiental_linha2_lme()
+    # con.pesquisa_ambiental_linha2_diario('2020-04-06')
+    con.pesquisa_ambiental_linha2_diario_total('2020-04-06')
